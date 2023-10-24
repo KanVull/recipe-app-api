33c2d9a6f240f702d7f86be5ad7a70a566e4d4a5
KanVull <minirorus2@gmail.com>
2023-10-24 17:30:25 +0400

    Added db service to docker-compose.

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x: int, y: int) -> int:
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """Subtract y from x and return result."""
+    return x - y

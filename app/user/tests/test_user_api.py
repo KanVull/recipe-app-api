d85eb60ac66efa31b720ce99e7f44290df3ac940
KanVull <minirorus2@gmail.com>
2023-11-27 13:57:02 +0400

    User creation class added and tested.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -53,7 +53,7 @@ class PublicUserApiTests(TestCase):
         """Test an error is returned if password less then 5 chars."""
         payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
+            'password': 'test',
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
